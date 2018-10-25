0f54600312483b48070c995cf54b0ab4f5c49180
Andrew Wilson <5093707+andwilson@users.noreply.github.com>
2018-10-25 10:28:08 -0700

    Completed config for manager app

---
+++ manager/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import firebase from "firebase";
+import reducers from "./reducers";
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyB7AKm8LQPWjZ2IkWEBp5rnuo9Pi6o-lGc",
+      authDomain: "manager-42326.firebaseapp.com",
+      databaseURL: "https://manager-42326.firebaseio.com",
+      projectId: "manager-42326",
+      storageBucket: "manager-42326.appspot.com",
+      messagingSenderId: "877727381483"
+    };
+    firebase.initializeApp(config);
+  }
+  
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Manager App!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
